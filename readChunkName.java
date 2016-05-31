1e9885fa4df78b5270f37418a41e8a124301e8d9
prithvikopplu <k.prithvireddy@gmail.com>
2016-05-31 19:14:57 -0400

    Add files via upload

---
+++ readChunkName.java
@@ -0,0 +1,28 @@
+package Peer1;
+import java.io.*;
+import java.util.*;
+public class readChunkName 
+{
+	public static ArrayList<String> readFileName(String file)
+	{
+		ArrayList<String>files=new ArrayList<String>();	
+		
+	try (BufferedReader br = new BufferedReader(new FileReader(file)))
+	{
+
+		String CurrentLine;
+
+		while ((CurrentLine = br.readLine()) != null)
+		{
+			files.add(CurrentLine);
+		}
+	}
+	catch (IOException e)
+	{
+		e.printStackTrace();
+	} 
+		
+		return files;
+}
+
+}
