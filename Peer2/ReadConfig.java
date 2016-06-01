3082988b00fb9254e1b6cc41ffca813ab3038832
prithvikopplu <k.prithvireddy@gmail.com>
2016-05-31 20:00:37 -0400

    Add files via upload

---
+++ Peer2/ReadConfig.java
@@ -0,0 +1,22 @@
+package Peer2;
+
+import java.io.*;
+public class ReadConfig {
+ 
+	public static String[] readport() {
+ 			String [] ports=new String[6] ;
+ 			int i = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader("src/Peer2/config.txt")))
+		{ 
+			String currentLine;
+ 			while ((currentLine = br.readLine()) != null) {
+				ports[i] = (currentLine);
+				i++;
+			}
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 		
+ 		return ports;
+	}
+}
